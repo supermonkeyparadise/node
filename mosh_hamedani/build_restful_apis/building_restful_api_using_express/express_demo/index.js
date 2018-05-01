1e7b84b8532f32ae47ca5957d4a29e10660e29fc
Unknown <steven.chou@uniqestate.com>
2018-05-01 22:48:45 +0800

    Handling HTTP PUT Requests

--- mosh_hamedani/build_restful_apis/building_restful_api_using_express/express_demo/index.js
+++ mosh_hamedani/build_restful_apis/building_restful_api_using_express/express_demo/index.js
@@ -34,18 +34,11 @@ app.get('/api/posts/:year/:month', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
+  const { error } = validateCourse(req.body);
 
-  const result = Joi.validate(req.body, schema);
-  console.log('Joi validate obj:', result);
-
-  if (result.error) {
+  if (error) {
     // 400 Bad Request
-    res.status(400).send(result.error.details[0].message);
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -56,6 +49,33 @@ app.post('/api/courses', (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send('The course with the given ID was not found!');
+  }
+
+  const { error } = validateCourse(req.body);
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+const validateCourse = course => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  return Joi.validate(course, schema);
+};
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
