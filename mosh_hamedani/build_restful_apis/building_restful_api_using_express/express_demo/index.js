dcc7d743a2eea0932dc7fffd7794256a770f1b9b
Unknown <steven.chou@uniqestate.com>
2018-05-01 21:24:17 +0800

    Handling HTTP GET Requests

--- mosh_hamedani/build_restful_apis/building_restful_api_using_express/express_demo/index.js
+++ mosh_hamedani/build_restful_apis/building_restful_api_using_express/express_demo/index.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' }
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-  res.send(req.params.id);
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (course) {
+    res.send(course);
+  } else {
+    res.status(404).send('The course with the given ID was not found!');
+  }
 });
 
+// route params & queryStr
 app.get('/api/posts/:year/:month', (req, res) => {
   res.send({ params: req.params, query: req.query });
 });
