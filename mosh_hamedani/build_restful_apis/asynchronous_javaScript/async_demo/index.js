0210a119dd8982a196b42629d2eb7f2acdaeca0c
Unknown <steven.chou@uniqestate.com>
2018-05-08 16:40:08 +0800

    Callbacks

--- mosh_hamedani/build_restful_apis/asynchronous_javaScript/async_demo/index.js
+++ mosh_hamedani/build_restful_apis/asynchronous_javaScript/async_demo/index.js
@@ -1,7 +1,28 @@
+const getUser = (id, callback) => {
+  setTimeout(() => {
+    console.log('Reading a user from a database...');
+    callback({ id: id, gitHubUsername: 'Steven' });
+  }, 2000);
+};
+
+const getRepositories = (username, callback) => {
+  setTimeout(() => {
+    console.log('Calling GitHub API...');
+    callback(['repo1', 'repo2', 'repo3']);
+  }, 2000);
+};
+
+// 三種解法
+// Callbacks
+// Promises
+// Async/await
+
 console.log('Before');
 
-setTimeout(() => {
-  console.log('Reading a user from a database...');
-}, 2000);
+getUser(1, user => {
+  getRepositories(user.gitHubUsername, repos => {
+    console.log(`Repos: ${repos}`);
+  });
+});
 
 console.log('After');
