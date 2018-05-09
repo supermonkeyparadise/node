1e78e056c9237116b4167d1bb404ed2d5a0e9a40
Unknown <steven.chou@uniqestate.com>
2018-05-09 11:22:46 +0800

    Async and Await

--- mosh_hamedani/build_restful_apis/asynchronous_javaScript/async_demo/async_await_approach.js
+++ mosh_hamedani/build_restful_apis/asynchronous_javaScript/async_demo/async_await_approach.js
@@ -1,32 +1,57 @@
 console.log('Before');
 
-// getUser(1, user => {
-//   getRepositories(user.gitHubUsername, repos => {
-//     getCommits(repos[0], commits => {
-//       console.log(commits);
-//     });
-//   });
-// });
+// [ Promise approach ]
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log('Commits:', commits))
+//   .catch(err => console.log('Error:', err.message));
+
+// [ Asycn & Await approach ] 語法糖果
+// 1. use await when return Promise obj
+// 2. 用一個 async function 包住 await statement
+async function displayCommits() {
+  try {
+    // ＊＊ 讓撰寫 async code 就像是寫 sync code ㄧ樣 ＊＊
+    const user = await getUser(1); // v8 會轉譯成類似 .then(user => getRepositories(user.gitHubUsername))
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    // holding promise err
+    console.log('Error', err.message);
+  }
+}
+displayCommits();
 
 console.log('After');
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from a database...');
-    callback({ id: id, gitHubUsername: 'mosh' });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    // kick off some async work
+    setTimeout(() => {
+      console.log('Reading a user from a database...');
+      resolve({ id: id, gitHubUsername: 'mosh' });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
+function getRepositories(username) {
+  // kick off some async work
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
 }
 
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['commit']);
-  }, 2000);
+function getCommits(repo) {
+  // kick off some async work
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['commit']);
+    }, 2000);
+  });
 }
