4033d8f5ea976d132a9e4171e3144bd7210b09f6
Unknown <steven.chou@uniqestate.com>
2018-05-10 18:54:30 +0800

    Regular Expressions

--- mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/index.js
+++ mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/index.js
@@ -38,6 +38,7 @@ async function createCourse() {
 }
 
 async function getCourse() {
+  // *** 屬性比大小 ***
   // eq(equal)
   // ne(not equal)
   // gt(greater than)
@@ -50,10 +51,16 @@ async function getCourse() {
   // or
   // and
 
+  // 正規表示式
+  // /pattern/   /^pattern/: 開頭   /pattern$/: 結尾
+
   const courses = await Course.find({ author: 'Mosh', isPublished: true })
-    // .find({price:{$gt: 10, $lte: 20}})
-    // .find({price: { $in: [10, 15, 20]}})
+    // .find({ price: { $gt: 10, $lte: 20 }})
+    // .find({ price: { $in: [10, 15, 20] }})
     // .or([{ author: 'Mosh' }, { isPublished: true }])
+    // .find({ author: /^Mosh/ })
+    // .end({ author: /Hamedani$/i })
+    // .find({ author: /.*Mosh.*/i })
     .limit(10)
     .sort({ name: 1 }) // 1 ===> 升冪; -1 ===> 降冪
     .select({ name: 1, tags: 1 }); // 設定要回傳的 properties
