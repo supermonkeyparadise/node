4388c5f3d67b7503b8629c487fe478496680294b
Unknown <steven.chou@uniqestate.com>
2018-05-10 18:24:13 +0800

    Comparison Query Operators

--- mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/index.js
+++ mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/index.js
@@ -38,10 +38,21 @@ async function createCourse() {
 }
 
 async function getCourse() {
+  // eq(equal)
+  // ne(not equal)
+  // gt(greater than)
+  // gte(greater than or equal to)
+  // lt(less than)
+  // lte(less than or equal to)
+  // in
+  // nin(not in)
+
   const courses = await Course.find({ author: 'Mosh', isPublished: true })
+    // .find({price:{$gt: 10, $lte: 20}})
+    // .find({price: { $in: [10, 15, 20]}})
     .limit(10)
     .sort({ name: 1 }) // 1 ===> 升冪; -1 ===> 降冪
-    .select({ name: 1, tags: 1 });  // 設定要回傳的 properties
+    .select({ name: 1, tags: 1 }); // 設定要回傳的 properties
 
   console.log(courses);
 }
