0c1d66b241e22e8ca1c21d91ee754d3589bacac7
Unknown <steven.chou@uniqestate.com>
2018-05-10 18:29:17 +0800

    Logical Query Operators

--- mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/index.js
+++ mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/index.js
@@ -47,9 +47,13 @@ async function getCourse() {
   // in
   // nin(not in)
 
+  // or
+  // and
+
   const courses = await Course.find({ author: 'Mosh', isPublished: true })
     // .find({price:{$gt: 10, $lte: 20}})
     // .find({price: { $in: [10, 15, 20]}})
+    // .or([{ author: 'Mosh' }, { isPublished: true }])
     .limit(10)
     .sort({ name: 1 }) // 1 ===> 升冪; -1 ===> 降冪
     .select({ name: 1, tags: 1 }); // 設定要回傳的 properties
