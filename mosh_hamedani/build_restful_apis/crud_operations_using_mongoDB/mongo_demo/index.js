29e24dc4e0543ee9d04fc9d20a2c761ec3030c9f
Unknown <steven.chou@uniqestate.com>
2018-05-10 17:07:12 +0800

    Schemas

--- mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/index.js
+++ mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/index.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose');
 
 // playground ===> database
 // 如果 db 不存在，會自行建置
+// return Promise
 mongoose
   .connect('mongodb://localhost/playground')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// collection ===> table(關聯式)
+// documents ===> row(關聯式)
+// schema ===> 規定 key value 的樣式
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
