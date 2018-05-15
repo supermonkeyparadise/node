df387302e55e879e4a0458bf292a25a64b8f2a28
Unknown <steven.chou@uniqestate.com>
2018-05-15 18:57:07 +0800

    Custom Validators

--- mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/validation.js
+++ mosh_hamedani/build_restful_apis/crud_operations_using_mongoDB/mongo_demo/validation.js
@@ -25,7 +25,15 @@ const courseSchema = new mongoose.Schema({
     required: true
   },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      validator: function(v) {
+        return v && v.length > 0;
+      },
+      message: 'A course should have at least one tag.'
+    }
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
   price: {
@@ -47,9 +55,9 @@ async function createCourse() {
   // 2. 利用 class 得到 一個 object
   const course = Course({
     name: 'React Course',
-    category: '_',
+    category: 'web',
     author: 'Mosh',
-    tags: ['react', 'frontend'],
+    tags: [],
     isPublished: true,
     price: 15
   });
