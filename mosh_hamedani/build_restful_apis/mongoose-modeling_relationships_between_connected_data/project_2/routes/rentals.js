7d21ed4f45a6586c2a94565b05aae53b4dfc1646
Unknown <steven.chou@uniqestate.com>
2018-05-17 17:31:03 +0800

    Transactions

--- mosh_hamedani/build_restful_apis/mongoose-modeling_relationships_between_connected_data/project_2/routes/rentals.js
+++ mosh_hamedani/build_restful_apis/mongoose-modeling_relationships_between_connected_data/project_2/routes/rentals.js
@@ -2,8 +2,11 @@ const { Rental, validate } = require('../models/rental');
 const { Movie } = require('../models/movie');
 const { Customer } = require('../models/customer');
 const mongoose = require('mongoose');
+const Fawn = require('fawn');
 const express = require('express');
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
   const rentals = await Rental.find().sort('-dateOut');
@@ -35,12 +38,20 @@ router.post('/', async (req, res) => {
       dailyRentalRate: movie.dailyRentalRate
     }
   });
-  rental = await rental.save();
 
-  movie.numberInStock--;
-  movie.save();
+  // ** two phase commit **
+  try {
+    // rentals is collection
+    // 會在 mongoDB 中，新增加一個 collection，來完成 two phase commit
+    new Fawn.Task()
+      .save('rentals', rental)
+      .update('movies', { _id: movie._id }, { $inc: { numberInStock: -1 } })
+      .run();
 
-  res.send(rental);
+    res.send(rental);
+  } catch (ex) {
+    res.status(500).send('Something failed.');
+  }
 });
 
 router.get('/:id', async (req, res) => {
