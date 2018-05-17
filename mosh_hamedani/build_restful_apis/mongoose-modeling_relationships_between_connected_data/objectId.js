8b3fbaf518f1fc92e60807411f5edf0b3be54845
Unknown <steven.chou@uniqestate.com>
2018-05-17 17:56:28 +0800

    ObjectID

---
+++ mosh_hamedani/build_restful_apis/mongoose-modeling_relationships_between_connected_data/objectId.js
@@ -0,0 +1,17 @@
+// _id: 5afd1d80377c580c6d93588b  ===> 幾乎是唯一值
+// _id 不是由 mongoDB 產生的，而是由 mongoDB driver 產生的
+// 所以一個 driver 可以對多個 mongoDB instance
+
+// 12 bytes
+// 4 bytes: timestamp
+// 3 bytes: machine identifier
+// 2 bytes: process identifier
+// 3 bytes: counter
+
+const mongoose = require('mongoose');
+const id = new mongoose.Types.ObjectId();
+console.log('## id:', id);
+console.log('## tiemstamp:', id.getTimestamp());
+
+const isValid = mongoose.Types.ObjectId.isValid('1224');
+console.log('## isValid:', isValid);
