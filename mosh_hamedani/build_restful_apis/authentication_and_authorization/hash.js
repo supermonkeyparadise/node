38b23363864abd54bde00321efd6770782134d6b
Unknown <steven.chou@uniqestate.com>
2018-05-19 14:32:11 +0800

    Hashing Passwords

---
+++ mosh_hamedani/build_restful_apis/authentication_and_authorization/hash.js
@@ -0,0 +1,11 @@
+const bcrypt = require('./vidly/node_modules/bcrypt');
+
+async function run() {
+  const salt = await bcrypt.genSalt(10);
+  const hashed = await bcrypt.hash('1234', salt);
+
+  console.log(salt);
+  console.log(hashed);
+}
+
+run();
