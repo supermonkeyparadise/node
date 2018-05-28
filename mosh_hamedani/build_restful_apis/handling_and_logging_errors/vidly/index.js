525553085b1b2ba82941886a4f23c4a7cd1e3b02
Unknown <steven.chou@uniqestate.com>
2018-05-28 18:11:40 +0800

    Handling Rejected Promises

--- mosh_hamedani/build_restful_apis/handling_and_logging_errors/vidly/index.js
+++ mosh_hamedani/build_restful_apis/handling_and_logging_errors/vidly/index.js
@@ -2,21 +2,23 @@ const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
-const express = require('express');
-const app = express();
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
   process.exit(1);
 }
 
-mongoose.connect('mongodb://localhost/vidly')
+mongoose
+  .connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
