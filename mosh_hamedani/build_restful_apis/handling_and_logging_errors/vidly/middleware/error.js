610346aedd9f163a6520a679979f6d1a6463e196
Unknown <steven.chou@uniqestate.com>
2018-05-29 14:05:11 +0800

    Express Async Errors

--- mosh_hamedani/build_restful_apis/handling_and_logging_errors/vidly/middleware/error.js
+++ mosh_hamedani/build_restful_apis/handling_and_logging_errors/vidly/middleware/error.js
@@ -1,4 +1,4 @@
 module.exports = function(err, req, res, next) {
   // Log the exception
-  res.status(500).send('Something failed.');
+  res.status(500).send('Something failed.....');
 };
--- mosh_hamedani/build_restful_apis/handling_and_logging_errors/vidly/routes/genres.js
+++ mosh_hamedani/build_restful_apis/handling_and_logging_errors/vidly/routes/genres.js
@@ -1,34 +1,52 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const router = express.Router();
 
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const { Genre, validate } = require('../models/genre');
-const asyncMiddleware = require('./../middleware/async');
+// const asyncMiddleware = require('./../middleware/async');
 
-const router = express.Router();
 
-router.get(
-  '/',
-  asyncMiddleware(async (req, res) => {
-    const genres = await Genre.find().sort('name');
-    res.send(genres);
-  })
-);
 
-router.post(
-  '/',
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+// 自行包裝 expcetion，但有點複雜
+// router.get(
+//   '/',
+//   asyncMiddleware(async (req, res) => {
+//     const genres = await Genre.find().sort('name');
+//     res.send(genres);
+//   })
+// );
 
-    let genre = new Genre({ name: req.body.name });
-    genre = await genre.save();
+// 自行包裝 expcetion，但有點複雜
+// router.post(
+//   '/',
+//   auth,
+//   asyncMiddleware(async (req, res) => {
+//     const { error } = validate(req.body);
+//     if (error) return res.status(400).send(error.details[0].message);
 
-    res.send(genre);
-  })
-);
+//     let genre = new Genre({ name: req.body.name });
+//     genre = await genre.save();
+
+//     res.send(genre);
+//   })
+// );
+
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name');
+  res.send(genres);
+});
+
+router.post('/', auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
+
+  res.send(genre);
+});
 
 router.put('/:id', async (req, res) => {
   const { error } = validate(req.body);
